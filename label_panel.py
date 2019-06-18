323d2c15d1002bd2c6c5fa595d34c1cf8ba636e9
Yichen He <csyichenhe@gmail.com>
2019-06-18 18:40:41 +0100

    init 2

---
+++ label_panel.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+In this example we draw 6 lines using
+different pen styles.
+
+Author: Jan Bodnar
+Website: zetcode.com
+Last edited: August 2017
+"""
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys,os
+
+import mainWin
+
+TEMP_SHAPE_LENGTH =10
+TEMP_DOUBLE_SHAPE_LENGTH = TEMP_SHAPE_LENGTH*2
+
+
+class RectwithInfo(QRect ):
+
+    def __init__(self, cx ,cy, width,height , name='' , info_dict = {}):
+
+        x = cx - width//2
+        y = cy - height//2
+        super().__init__(x, y, width, height)
+
+        self.name = name
+        self.info_dict = info_dict
+
+
+class LabelPanel(QWidget):
+
+    def __init__(self,  data = None):
+        super().__init__()
+
+        self.initUI(data = data)
+
+
+    def initUI(self, data ):
+
+        self.state_moving = False
+        self.state_highlight = False
+
+        if data is not None:
+            self.data = data
+        # self.data.get_current_image().set_current_pt(x=7,y=8)
+
+        act_origin_size = QAction("origin", self, shortcut="Ctrl+o",
+                                      triggered=self.origin)
+
+        act_zoom_in = QAction("Zoom &In", self, shortcut="Ctrl+R",
+                                      triggered=self.zoom_in)
+        act_zoom_out = QAction("Zoom &In", self, shortcut="Ctrl+F",
+                                      triggered=self.zoom_out)
+
+        self.addAction(act_zoom_in)
+        self.addAction(act_zoom_out)
+        self.addAction(act_origin_size)
+
+        self.pixmap = None
+        self.scale = 1
+        self.offset = QPointF(0, 0)
+
+
+        self.highlight_shapes=[]
+
+        # scroll = QScrollArea()
+        # scroll.setWidget(self)
+        # scroll.setWidgetResizable(True)
+
+        self.setMouseTracking(True)
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('Pen styles')
+        self.show()
+
+    def init_for_testing(self):
+
+        file_name = 'data/data_file.json'
+        work_dir = './data/'
+
+        self.data = mainWin.Data(file_name,work_dir)
+
+        self.update()
+
+
+    def mousePressEvent(self, e):
+        pos = self.coords_tranfrom_widget_to_image(e.pos())
+
+        self.data.get_current_image().set_current_highlight_id(None)
+
+        if e.button() == Qt.LeftButton:
+            # if it is on the a currrent point
+            for idx, pt, in enumerate(self.data.get_current_scaled_points()):
+                if pt.rect.contains(pos):
+                    self.open_state_moving()
+                    self.state_highlight = True
+                    self.data.get_current_image().set_current_pt_id(idx)
+                    self.data.get_current_image().set_current_highlight_id(idx)
+
+
+            # if self.parent():
+            #     self.parent().window().list_properties()
+
+
+        #transform coord to image-wise coord
+
+        # self.points_list.append(e.pos())
+        #
+        # bbox = QRect(0 , 0,TEMP_SHAPE_LENGTH ,TEMP_SHAPE_LENGTH)
+        # bbox.moveCenter(e)
+        # self.bbox_list.append(bbox)
+
+        # select_
+        self.update_in_parent(pos)
+        self.update()
+
+
+    def mouseReleaseEvent(self, e):
+        pos = self.coords_tranfrom_widget_to_image(e.pos())
+        if e.button() == Qt.LeftButton:
+            # if it is on the a currrent point
+            self.close_state_moving()
+        self.update_in_parent(pos)
+        self.update()
+
+    def mouseMoveEvent(self, e):
+        pos = self.coords_tranfrom_widget_to_image(e.pos())
+
+
+        # if self.parent():
+        #     parent = self.parent().window()
+        #
+        #     parent.label_xy_status.setText('X: %d; Y: %d' % (pos.x(), pos.y()))
+
+
+
+        is_image_contain = self.pixmap.rect().contains(pos)
+
+        # print("image size", self.pixmap.rect().size())
+        # print("size:", self.size())
+        # print("origin: {}.   trans_pos:{}".format(e.pos() , pos))
+        if self.state_moving:
+            x = max(min(self.pixmap.rect().width(), pos.x()),0)
+            y = max(min(self.pixmap.rect().height(), pos.y()), 0)
+
+            # Unscaled the position and update the result.
+            self.data.get_current_image().set_current_pt(x = x , y= y , scale = self.data.scale)
+
+
+                #
+                # if self.parent():
+                #     self.parent().window().list_properties()
+
+
+        # self.highlight_shapes = []
+        # for bbox in self.bbox_list:
+        #     if bbox.contains(pos):
+        #         print(pos)
+        #         self.highlight_shapes.append(bbox)
+        self.update_in_parent(pos)
+        self.update()
+
+    def update_in_parent(self, pos):
+        if self.parent():
+            parent = self.parent().window()
+
+            scale = self.data.scale
+            parent.label_xy_status.setText('X: %d; Y: %d' % (pos.x()//scale, pos.y()//scale))
+            parent.statusBar().showMessage("{}%".format(round(self.data.scale,2)*100))
+            parent.list_properties()
+
+
+    def paintEvent(self, e):
+
+        painter = QPainter(self)
+
+        # Draw image part
+        if self.data is not None:
+            # self.pixmap = QPixmap(os.path.join(self.data.work_dir,self.data.get_current_image_name()))
+            self.draw_image(painter)
+            # if self.pixmap:
+            #     # painter.scale(self.scale, self.scale)
+            #     # self.offset = self.translate_to_center()
+            #     # painter.translate(self.translate_to_center())
+            #     self.draw_image(painter)
+            #     # painter.scale(1/self.scale, 1/self.scale)
+            # # print(pixmap.width(),pixmap.height())
+            #
+
+            # Draw annotations:
+            pen = QPen(Qt.red, 2)
+            brush = QBrush(QColor(0, 255, 255, 120))
+            painter.setPen(pen)
+            painter.setBrush(brush)
+
+            # length = max(self.size().width() , self.size().height())
+            # proportion =0.05
+            # length = int(proportion * length)
+            # From the scale point , draw points
+            for pt in self.data.get_current_scaled_points():
+                bbox = pt.rect
+                print("rect size",bbox.size())
+                # QRect(pt.rect.topLeft(), QSize(length,length))
+                self.draw_point(painter ,bbox)
+
+
+            #Setting for drawings Highlight:
+            pen = QPen(Qt.red, 4)
+            brush = QBrush(QColor(0, 255, 255, 200))
+            painter.setPen(pen)
+            painter.setBrush(brush)
+
+            highlight_bbox = self.data.get_current_image().get_current_highlight_bbox(scale = self.data.scale)
+            if highlight_bbox:
+                painter.drawRect(highlight_bbox)
+
+
+            # if self.state_highlight:
+            #     selected_bbox = self.data.get_current_image().get_current_pt().rect
+            #     painter.drawRect(selected_bbox)
+
+
+    def draw_image(self,painter):
+        # painter.drawPixmap(self.rect(), self.pixmap)
+        # self.pixmap.scaled(self.pixmap.size()*self.scale)
+        # print(self.pixmap.size()*self.scale)
+
+        # Check the
+
+
+        #Check the size of pixmap first:
+        # limit_size = QSize(1300,700)
+        # self.pixmap = self.data.current_pixmap
+
+        # Scale the size first from pixmap:
+        # self.pixmap = self.pixmap.scaled(self.scale *  self.pixmap.size() , Qt.KeepAspectRatio)
+        #
+        # if self.pixmap.width()>limit_size.width() or self.pixmap.height()>limit_size.height():
+        #
+        #     self.pixmap  = self.pixmap.scaled(limit_size, Qt.KeepAspectRatio )
+        #     print("resize map" , self.pixmap.size())
+
+        self.pixmap = self.data.get_current_scaled_pixmap()
+
+        size = self.size()
+        img_size = self.pixmap.size()
+
+        if size != img_size:
+            self.resize(img_size)
+
+        painter.drawPixmap(0,0, self.pixmap)
+
+    def draw_point(self,painter, bbox):
+        # painter.drawEllipse(bbox.center(), TEMP_SHAPE_LENGTH/2 * 1/self.scale, TEMP_SHAPE_LENGTH/2 * 1/self.scale)
+        painter.drawEllipse(bbox.center(), bbox.width()//2, bbox.height()//2)
+
+
+    def zoom_in(self):
+        self.data.set_scale(1.25)
+
+        self.update()
+        # new_size = self.scale * self.pixmap.size()
+        # self.resize(new_size)
+        # self.update()
+
+    def zoom_out(self):
+        self.data.set_scale(0.8)
+        self.update()
+
+    def origin(self):
+        self.data.reset_scale()
+        self.update()
+
+    def coords_tranfrom_widget_to_image(self,pos):
+        s = self.scale
+        offset = self.offset
+        new_x = pos.x()/s
+        new_y = pos.y()/s
+        new_x -=offset.x()
+        new_y -=offset.y()
+        return QPoint(int(new_x) , int(new_y))
+
+    def translate_to_center(self):
+        s = self.scale
+        area = super(LabelPanel, self).size()
+        w, h = self.pixmap.width() * s, self.pixmap.height() * s
+        aw, ah = area.width(), area.height()
+        x = (aw - w) / (2 * s) if aw > w else 0
+        y = (ah - h) / (2 * s) if ah > h else 0
+        return QPointF(x, y)
+
+    def toggle_state_moving(self):
+        self.state_moving = not self.state_moving
+
+    def open_state_moving(self):
+        self.state_moving = True
+
+    def close_state_moving(self):
+        self.state_moving = False
+
+    # def paintEvent(self, event):
+    #     painter = QPainter(self)
+    #     pixmap = QPixmap("large.jpg")
+    #     painter.drawPixmap(self.rect(), pixmap)
+    #     # pen = QPen(Qt.red, 3)
+    #
+    #
+    #     pen = QPen(Qt.red, 2)
+    #     brush = QBrush(QColor(0, 255, 255, 120))
+    #     painter.setPen(pen)
+    #     painter.setBrush(brush)
+    #
+    #     for i in range(self.points.count()):
+    #         painter.drawEllipse(self.points.point(i), 5, 5)
+    #
+    #     # painter.drawLine(10, 10, self.rect().width() -10 , 10)
+    #
+    #
+    #
+    #     # painter.drawRect(40, 40, 400, 200)
+
+    # def paintEvent(self, e):
+    #
+    #     qp = QPainter()
+    #     qp.begin(self)
+    #     pixmap = QPixmap("small.png")
+    #     qp.drawPixmap(self.rect(), pixmap)
+    #     self.drawLines(qp)
+    #     qp.end()
+    #
+    #
+    # def drawLines(self, qp):
+    #
+    #     pen = QPen(Qt.black, 2, Qt.SolidLine)
+    #
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 40, 250, 40)
+    #
+    #     pen.setStyle(Qt.DashLine)
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 80, 250, 80)
+    #
+    #     pen.setStyle(Qt.DashDotLine)
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 120, 250, 120)
+    #
+    #     pen.setStyle(Qt.DotLine)
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 160, 250, 160)
+    #
+    #     pen.setStyle(Qt.DashDotDotLine)
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 200, 250, 200)
+    #
+    #     pen.setStyle(Qt.CustomDashLine)
+    #     pen.setDashPattern([1, 4, 5, 4])
+    #     qp.setPen(pen)
+    #     qp.drawLine(20, 240, 250, 240)
+
+
+
+    ### Deprecated funs ###
+
+    # # Setting the image and annotation for labelling.
+    # def open_image(self , file_name = None):
+    #     if file_name:
+    #         self.pixmap = QPixmap(file_name)
+    #         self.update()
+    #
+    # def set_annotations(self, pt_list):
+    #
+    #     if pt_list:
+    #         self.selected_bbox_id = None
+    #         self.bbox_list = []
+    #         for pt in pt_list:
+    #             rect_temp = RectwithInfo(pt.x , pt.y ,TEMP_SHAPE_LENGTH , TEMP_SHAPE_LENGTH, pt.pt_name)
+    #             self.bbox_list.append(rect_temp)
+    #         self.update()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = LabelPanel()
+    ex.init_for_testing()
+    sys.exit(app.exec_())
---
+++ relabelData.py
@@ -0,0 +1,251 @@
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QPixmap
+import os
+from pathlib import Path
+import json
+from math import ceil
+
+TEMP_SHAPE_LENGTH =111
+
+pixmap_size_limit = QSize(1300,700)
+
+
+class Point():
+    def __init__(self, pt_name, x, y, width = TEMP_SHAPE_LENGTH, error = False,info = None):
+        self.pt_name = pt_name
+        self.x = x
+        self.y = y
+        self.info = info
+        self.error = error
+
+        self.set_rect(self.x,self.y, width, width)
+
+
+    def set_rect(self,cx,cy,width,height):
+
+
+        x = cx - width//2
+        y = cy - height//2
+        self.rect = QRect(x, y, width, height)
+
+    def set_point(self, pt_name = None,x= None,y=None, width = None, error = None,info = None):
+        if pt_name is not None:
+            self.pt_name = pt_name
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+        if error is not None:
+            self.error = error
+        if info is not None:
+            self.info = info
+
+        if width is None:
+            width = self.rect.width()
+
+
+
+        self.set_rect(self.x,self.y, width, width)
+
+    def __mul__(self, factor):
+        new_x = self.x * factor
+        new_y = self.y * factor
+        new_width = self.rect.width() *factor
+
+        return Point(self.pt_name, x=new_x, y=new_y, width=new_width, error=self.error, info=self.info)
+
+
+
+class Image():
+    current_pt_id = None
+    def __init__(self, img_name, pt_lists=None):
+        self.img_name = img_name
+
+        if pt_lists:
+            self.points = []
+            for pt in pt_lists:
+                point = Point(pt['name'], pt['x'] , pt['y'])
+                self.points.append(point)
+
+            self.current_pt_id = 0
+            self.pt_size = len(self.points)
+            self.current_highlight_id = None
+
+    def get_current_highlight_bbox(self, scale= None):
+        if self.current_highlight_id is not None:
+            if scale is not None:
+                return (self.points[self.current_highlight_id] * scale).rect
+            else:
+                return self.points[self.current_highlight_id].rect
+        else:
+            return None
+    def set_current_highlight_id(self, idx):
+        self.current_highlight_id = idx
+
+    def next_point(self):
+        if self.current_pt_id<self.pt_size-1:
+            self.current_pt_id = (self.current_pt_id+1)
+
+    def prev_image(self):
+        if self.current_pt_id>0:
+            self.current_pt_id = (self.current_pt_id-1)
+
+
+    def set_current_pt_id(self, idx):
+        self.current_pt_id = idx
+
+    def set_current_pt(self, pt_name = None,x= None,y=None, error = None,info = None, scale=1):
+        self.points[self.current_pt_id].set_point(pt_name = pt_name, x= x//scale, y=y//scale, error = error,info = info)
+
+
+    def set_current_pt_x(self, x):
+        self.points[self.current_pt_id].x = x
+
+    def set_current_pt_y(self, y):
+        self.points[self.current_pt_id].y = y
+
+    def set_points_width(self,width):
+        for pt in self.points:
+            pt.set_point(width=width)
+
+    def get_current_pt_name(self):
+        return self.points[self.current_pt_id].pt_name
+
+    def get_current_pt_id(self):
+        return self.current_pt_id
+
+    def get_current_pt(self):
+        return self.points[self.current_pt_id]
+
+    def get_current_pt_x(self):
+        return self.points[self.current_pt_id].x
+
+    def get_current_pt_y(self):
+        return self.points[self.current_pt_id].y
+
+
+
+rect_length_prop = 0.015
+class Data():
+
+    def __init__(self, file_name, work_dir="."):
+
+        self.work_dir = work_dir
+        self.file_name = file_name
+
+        self.init_images()
+
+
+    def init_images(self):
+        self.images = []
+        work_dir_file_names = [os.path.basename(file_name) for file_name in Path(self.work_dir).glob('*')]
+
+        with open(self.file_name, "r") as read_file:
+            data = json.load(read_file)
+        # Init images list
+        for entry in data:
+            if entry['file_name'] in work_dir_file_names:
+                image = Image(entry['file_name'] , entry['points'])
+                self.images.append(image)
+
+        self.current_image_id = 0
+        self.img_size = len(self.images)
+
+        self.current_pixmap = QPixmap(os.path.join(self.work_dir,self.get_current_image_name()))
+        self.set_scale_fit_limit()
+        # Read and set the scale if it is too large
+
+
+
+    # def next_image(self):
+    #     if self.current_image_id<self.img_size-1:
+    #         self.current_image_id = (self.current_image_id+1)
+    #
+    # def prev_image(self):
+    #     if self.current_image_id>0:
+    #         self.current_image_id = (self.current_image_id-1)
+
+    def set_image_id(self, idx):
+        self.current_image_id = idx
+        # Reset scale and current pixmap
+
+        self.current_pixmap = QPixmap(os.path.join(self.work_dir,self.get_current_image_name()))
+        self.set_scale_fit_limit()
+
+    def set_work_dir(self,work_dir):
+        self.work_dir = work_dir
+        self.init_images()
+
+    def set_file_name(self,file_name):
+        self.file_name = file_name
+        self.init_images()
+
+    def set_scale(self,scale):
+        self.scale*=scale
+
+    def reset_scale(self):
+        self.scale = self.origin_scale
+
+    def set_scale_fit_limit(self):
+        if self.current_pixmap.width()>pixmap_size_limit.width() or self.current_pixmap.height()>pixmap_size_limit.height():
+            width_scale = ceil(self.current_pixmap.width()/pixmap_size_limit.width())
+            height_scale = ceil(self.current_pixmap.height()/pixmap_size_limit.height())
+
+            self.set_scale(1/max(width_scale,height_scale))
+        else:
+            self.scale = 1
+        self.origin_scale = self.scale
+
+        size = self.current_pixmap.size()
+          # length = max(self.size().width() , self.size().height())
+            # proportion =0.05
+        length = int(rect_length_prop * max(size.width(),size.height()))
+        print(length)
+        self.get_current_image().set_points_width(length)
+
+
+
+
+    def get_current_image(self):
+        return self.images[self.current_image_id]
+
+    def get_current_image_points(self):
+        return self.images[self.current_image_id].points
+
+    def get_current_image_name(self):
+        return self.images[self.current_image_id].img_name
+
+    def get_current_image_abs_path(self):
+        path = os.path.join(self.work_dir , self.images[self.current_image_id].img_name)
+        path = os.path.abspath(path)
+        return path
+
+    def get_image_id(self):
+        return self.current_image_id
+
+    def get_current_scaled_pixmap(self):
+        return self.current_pixmap.scaled(self.scale *  self.current_pixmap.size() , Qt.KeepAspectRatio)
+
+    def get_current_scaled_points(self):
+        return [pt * self.scale for pt in self.get_current_image_points()]
+
+    def write_json(self, save_name = None):
+        # Create data form to save
+        image_list = []
+        for image in self.images:
+            entry = {'file_name': image.img_name}
+            points = []
+            for pt in image.points:
+                pt_data = {"name": pt.pt_name, "x": pt.x, "y": pt.y, "info": pt.info,"error": pt.error}
+                points.append(pt_data)
+            entry['points'] = points
+            image_list.append(entry)
+
+        if save_name is None:
+            save_name = self.file_name
+
+        with open(save_name, 'w') as write:
+            json.dump(image_list, write)
+
